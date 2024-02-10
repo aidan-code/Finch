2499af1c5aa55768dc07ef0d71fd2156057f4536
Raymond Mbam <47081168+raymondmbam@users.noreply.github.com>
2024-02-10 15:34:44 +0000

    feat: Add assistantLogo.css, add assistantLogo.js, add input.css, add input.js, add voiceButton.css and add voiceButton.js

---
+++ src/Components/text/assistantLogo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./assistantLogo.css";
+import AssistantLogoIcon from "../../Assets/AssistantLogo.svg";
+
+export const AssistantLogo = () => {
+  return (
+    <div className="assistant-frame">
+      <img className="group" alt="Group" src={AssistantLogoIcon} />
+      <div className="text-wrapper">Assistant</div>
+    </div>
+  );
+};
+
+export default AssistantLogo;
