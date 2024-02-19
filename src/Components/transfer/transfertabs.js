d089a04bed982f9a47b50d0011a27a8eef9fb2d2
Raymond Mbam <raymondmbam@gwmail.gwu.edu>
2024-02-19 02:52:11 +0000

    hi

---
+++ src/Components/transfer/transfertabs.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import "./transfertabs.css";
+
+const Tabs = ({ items }) => {
+  const [selectedTabIndex, setSelectedTabIndex] = useState(0);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleTabClick = (index) => {
+    setSelectedTabIndex(index);
+  };
+
+  const filteredItems = items.filter((item) =>
+    item.name.toLowerCase().includes(searchTerm.toLowerCase()),
+  );
+
+  return (
+    <div className="tabs-container">
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="search-input"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+      <div className="tabs">
+        {filteredItems.map((item, index) => (
+          <div
+            key={index}
+            className={`tab ${index === selectedTabIndex ? "selected" : ""}`}
+            onClick={() => handleTabClick(index)}
+          >
+            <img src={item.imageUrl} alt={item.name} className="tab-image" />
+            <p className="tab-name">{item.name}</p>
+            {index === selectedTabIndex && <div className="selected-line" />}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Tabs;
--- src/Pages/Transfer.js
+++ src/Pages/Transfer.js
@@ -1,69 +1,32 @@
 import React from "react";
 import "./Transfer.css";
+import Tabs from "../Components/transfer/transfertabs";
+import Sam from "../Assets/revolut.jpg";
+import James from "../Assets/wise.jpg";
+import Elvis from "../Assets/finch.jpg";
+import Josephine from "../Assets/zelle.jpg";
+
+const items = [
+  { name: "Sam", imageUrl: Sam },
+  { name: "James", imageUrl: James },
+  { name: "Elvis", imageUrl: Elvis },
+  { name: "Josephine", imageUrl: Josephine },
+  // Add more items as needed
+];
 
 export const Transfer = () => {
   return (
-    <div className="transfer-incomplete">
-      <div className="div">
-        <div className="overlap-group">
-          <img className="background" alt="Background" src="background.png" />
-          <div className="frame" />
-          <div className="frame-wrapper">
-            <div className="frame-2">
-              <div className="text-wrapper">$</div>
-              <p className="element">
-                <span className="span">50.</span>
-                <span className="text-wrapper-2">00</span>
-              </p>
-            </div>
+    <div className="transfer-home">
+      <div className="transfer">
+        <div className="div">
+          <div>
+            <Tabs items={items} />
           </div>
-          <div className="frame-3">
-            <div className="frame-4">
-              <div className="frame-5">
-                <div className="frame-6">
-                  <img className="img" alt="Revolut" src="revolut.png" />
-                  <div className="text-wrapper-3">Sam</div>
-                </div>
-                <div className="rectangle" />
-              </div>
-              <div className="frame-5">
-                <div className="frame-6">
-                  <img className="img" alt="Wise" src="wise.png" />
-                  <div className="text-wrapper-3">James</div>
-                </div>
-                <div className="rectangle-2" />
-              </div>
-              <div className="frame-5">
-                <div className="frame-6">
-                  <img className="img" alt="Finch" src="finch.png" />
-                  <div className="text-wrapper-3">Elvis</div>
-                </div>
-                <div className="rectangle" />
-              </div>
-              <div className="frame-5">
-                <div className="frame-6">
-                  <img className="img" alt="Zelle" src="zelle.png" />
-                  <div className="text-wrapper-3">Josephine</div>
-                </div>
-                <div className="rectangle" />
-              </div>
-            </div>
-            <div className="rectangle-3" />
-          </div>
-          <div className="div-wrapper">
-            <div className="text-wrapper-4">Send</div>
-          </div>
-        </div>
-        <div className="frame-7">
-          <div className="text-wrapper-5">Transfer</div>
-          <img
-            className="eva-arrow-back"
-            alt="Eva arrow back"
-            src="eva-arrow-back-outline.svg"
-          />
-          <div className="frame-8">
-            <div className="text-wrapper-6">Search contact</div>
-          </div>
+          <div className="text-wrapper">$</div>
+          <p className="element">
+            <span className="span">50.</span>
+            <span className="text-wrapper-2">00</span>
+          </p>
         </div>
       </div>
     </div>
